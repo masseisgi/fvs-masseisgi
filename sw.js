e2b20e8b704e973955d7f3f78a56d0c80d933de2
masseisgi <thiago@masseisgi.com>
2026-06-11 09:13:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 // IMPORTANTE: ao publicar nova versão do app, incremente este número.
 // Isso força todos os dispositivos a baixarem a versão nova (limpa cache antigo).
-const CACHE_NAME = 'mgi-pro-v10';
+const CACHE_NAME = 'mgi-pro-v11';
 
 const ASSETS = [
   './manifest.json',
